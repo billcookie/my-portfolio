1ec07f59a7350c1586d54c640837a4ef0431c099
Bill Cook <billcook8122@gmail.com>
2022-09-08 16:03:47 +0900

    added more visuals

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,53 +5,20 @@ export default function Contact() {
     <section id="contact" className="relative">
       <div data-aos="fade-down" data-aos-duration="2000">
         <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap justify-center">
-
-          {/* <i class="fa-solid fa-paper-plane text-6xl flex p-4 items-center"></i> */}
-          <form
-            netlify
-            name="contact"
-            className="lg:w-1/3 md:w-1/2 flex flex-col justify-center">
-            <div className="relative mb-4">
-              <label htmlFor="name" className="leading-7 text-sm text-gray-400">
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <div className="relative mb-4">
-              <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <div className="relative mb-4">
-              <label
-                htmlFor="message"
-                className="leading-7 text-sm text-gray-400">
-                Message
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <button
-              type="submit"
-              className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-              Send
-            </button>
-          </form>
+          <h2 className="title-font sm:text-4xl text-4xl mb-4 font-medium text-white">
+            Contact me
+            {/* <br className="hidden lg:inline-block" />I love to build amazing
+            apps. */}
+          </h2>
         </div>
+        <div className="px-5 py-10 flex justify-center ">
+          <p>Whether it is just to chat or to collab, please feel free to reach out to me on linkedin or Github!</p>
+        </div>
+        <div className="px-5 py-10 flex justify-center ">
+          <a href="https://www.linkedin.com/in/bill--cook/"><i class="devicon-linkedin-plain text-6xl flex p-4 items-center"></i></a>
+          <a href="https://github.com/billcookie"><i class="devicon-github-original-wordmark text-6xl flex p-4 items-center"></i></a>
+        </div>
+
       </div>
     </section>
   );
