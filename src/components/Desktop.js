cab0bc4904fcf2001d90f157f613506a1f8e3617
Bill Cook <billcook8122@gmail.com>
2022-09-29 14:08:36 +0900

    added one more link for guitar for desktop

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -73,7 +73,7 @@ export default function Mobile() {
             <div class="w-full md:w-1/3 pr-4 pl-4 lg:w-1/2 pr-4 pl-4">
               <img src="./Bills-guitars-screenshot.png" class=" max-w-full h-auto site-image" alt="image of tofuture home site" />
               <div className="flex justify-between">
-                    <a href="https://shopify-guitar.vercel.app/"className="mt-1 bg-blue-500 w-32 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded-full"><i class="fa-solid fa-globe"></i> Site</a>
+                    <a href="https://bills-guitars.vercel.app/"className="mt-1 bg-blue-500 w-32 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded-full"><i class="fa-solid fa-globe"></i> Site</a>
                     <a href="https://github.com/billcookie/shopify-guitar"className="mt-1 bg-blue-500 w-32 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded-full"><i class="fa-solid fa-code"></i> Code</a>
               </div>
             </div>
